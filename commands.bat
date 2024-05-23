ad25c90945892002bfa3522d04bf2e78e127ed3a
akhilamitineni <amitineni.pegasian@gmail.com>
2024-05-23 13:36:04 +0530

    Added my project

--- commands.bat
+++ commands.bat
@@ -1,6 +1,6 @@
 call git init
 call git add .
-call git commit -m 'Added my project'
+call git commit -m "Added my project"
 call git branch -M AKHIL_FILES
 call git remote set-url origin https://github.com/akhilamitineni/My-files.git
 call git push -u origin AKHIL_FILES
